143789bb4eb080f0a40350e5e50c57d128b357a0
maruf <maruf.csea1@gmail.com>
2019-01-23 18:06:56 -0500

    First Commit

--- main.cpp
+++ main.cpp
@@ -224,7 +224,7 @@ int main(int argc, char** argv) {
 	double elapsed_time = ((end_comm.tv_sec*1000 + (end_comm.tv_nsec/1.0e6)) - (start_comm.tv_sec*1000 + (start_comm.tv_nsec/1.0e6)));
 
 	time(&time_end);
-	logFile<<graphName.substr (6, (graphName.length() - 9))<<","<<elapsed_time<<","<<prev_mod<<std::endl;
+	logFile<<graphName.substr (6, (graphName.length() - 10))<<","<<elapsed_time<<","<<prev_mod<<std::endl;
 
 	t2 = clock();
 	float diff = ((float) t2 - (float) t1);
